93746c71a4294f048bb8908992cd0b851b26e4ed
EC2 Default User <126998671+fuguita@users.noreply.github.com>
2023-03-21 14:05:53 +0000

    kadai2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,42 @@
 class BooksController < ApplicationController
+
   def new
+    @book = Book.new
   end
 
+  def create
+    @book = Book.new(book_params)
+    @book.save
+    redirect_to book_path(@book.id)
+  end
+
+
   def index
+    @books = Book.all
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
+    @book = Book.find(params[:id])
+
   end
+
+  def update
+    book = find(params[:id])
+    book.update(list_params)
+    redirect_to book_path(iist.id)
+
+
+  end
+
+
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
 end
